aaf957070a5159d6f94167a00bf8a7f5d5b9c0f2
harshinichella <59983497+harshinichella@users.noreply.github.com>
2020-10-07 17:33:35 -0500

    adjust js document to create a message when clicking submit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 function init(){
+  var submit = document.getElementById("submit"); 
+  button.addEventListener('click', function() {
+    alert("Success! You have been added to the Evoke Cyber mailing list.");
+  });
 }
