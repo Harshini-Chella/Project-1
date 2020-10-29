af2af039fd44b0be30d9a140b9c2be5a761cb9ea
harshinichella <59983497+harshinichella@users.noreply.github.com>
2020-10-28 20:39:03 -0500

    add validate form to name field

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-function init(){
-  var submit = document.getElementById("submit"); 
-  submit.addEventListener('click', function() {
-    alert("Success! You have been added to the Evoke Cyber mailing list.");
-  });
-}
+function validateForm() {
+var x = document.forms["myForm"]["name"].value;
+  if (x === "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  else{
+  alert("Success! You have been added to the Evoke Cyber mailing list.");
+  }
+} 
 
-window.addEventListener('load', init);
